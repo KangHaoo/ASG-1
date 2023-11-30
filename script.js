7fdbe16c931787390936bf5f0c5bf64f1ffe542d
KangHaoo <s10257474@connect.np.edu.sg>
2023-12-01 02:06:34 +0800

    completed main page, by adding slide show and words.

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+let slideIndex = 0;
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slideIndex++;
+  
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change slide every 2 seconds
+}
+
+function changeSlide(n) {
+  slideIndex += n;
+  showSlides();
+}
+
+document.addEventListener("DOMContentLoaded", showSlides);
